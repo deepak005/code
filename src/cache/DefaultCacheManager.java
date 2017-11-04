b5088625c1c2f6b4e5bf1d0c16bbbc614ef79808
Deepak Agrawal <dagrawal@mobileiron.com>
2017-11-04 15:39:49 +0530

    Using java read write lock instead of sync

--- src/cache/DefaultCacheManager.java
+++ src/cache/DefaultCacheManager.java
@@ -2,11 +2,17 @@ package cache;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DefaultCacheManager<K, V> extends AbstractCacheManager<K, V> {
 
-
     private final Map<K, V> cache;
+
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private final Lock readLock = readWriteLock.readLock();
+    private final Lock writeLock = readWriteLock.writeLock();
 
     public DefaultCacheManager(long size) {
         super(size, CacheType.DEFAULT);
@@ -15,35 +21,51 @@ public class DefaultCacheManager<K, V> extends AbstractCacheManager<K, V> {
         cache = new HashMap<>();
     }
 
-    /**
-     * TODO: Should we use better locking than synchronized?
-     */
     @Override
-    public synchronized void put(K k, V v) {
-        if (cache.size() == getSize()) {
-            throw new CacheRuntimeException(CacheExceptionCode.CACHE_FULL, null, getSize());
+    public void put(K k, V v) {
+        writeLock.lock();
+        try {
+            if (cache.size() == getSize()) {
+                throw new CacheRuntimeException(CacheExceptionCode.CACHE_FULL, null, getSize());
+            }
+
+            this.cache.put(k, v);
+        } finally {
+            writeLock.unlock();
         }
-
-        this.cache.put(k, v);
-
 
     }
 
     @Override
     public synchronized void delete(K k) {
-        this.cache.remove(k);
+        writeLock.lock();
+        try {
+            this.cache.remove(k);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     @Override
     public V get(K k) {
-        return this.cache.get(k);
+        readLock.lock();
+        try {
+            return this.cache.get(k);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public void logStats() {
-       super.logStats();
+        readLock.lock();
+        try {
+            super.logStats();
 
-       System.out.println("Cache filled size = " + this.cache.size());
+            System.out.println("Cache filled size = " + this.cache.size());
+        } finally {
+            readLock.unlock();
+        }
     }
 
 }
